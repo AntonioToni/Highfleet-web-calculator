392a31e0f48e793ebb8ab2b974722c142187e829
Antonio Toni <antonio.gaming10@gmail.com>
2023-02-09 13:27:03 +0100

    Basic layout of website (#2)

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
-import { useState } from 'react'
+import { Container, Typography } from '@mui/material'
+import { IO } from './components/IO'
 
 function App() {
 
   return (
     <>
+      <Container sx={{display: 'flex', flexDirection: 'column', minHeight: '100vh', alignItems:'center'}}>
+        <Typography variant='h3' sx={{marginTop: 2}}>HIGHFLEET FUEL CALCULATOR</Typography>
+        <div style={{justifyContent:'center', alignItems:'center', flex: '1 0 0%', display: 'flex'}}>
+          <IO/>
+        </div>
+      </Container>
     </>
   )
 }
