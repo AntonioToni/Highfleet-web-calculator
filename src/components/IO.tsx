9280fda4f5163e63c9b0ed8f81dd6e91aad2d673
Antonio <antonio.gaming10@gmail.com>
2023-02-10 04:24:18 +0100

    Image now changes with select, number with input

--- src/components/IO.tsx
+++ src/components/IO.tsx
@@ -2,14 +2,13 @@ import { MenuItem, Select, SelectChangeEvent, Stack, TextField, InputLabel, Form
 import { useState } from "react"
 import Sevastopol from "../assets/Sevastopol.png";
 import Negev from "../assets/Negev.png";
+import Nomad from "../assets/Nomad.png";
+import Lightning from "../assets/Lightning.png";
+import "../ships.json";
 
 export function IO() {
-  const [ship, setShip] = useState('');
-  const [distance, setDistance] = useState('');
-
-  const handleChange = (event: SelectChangeEvent) => {
-    setShip(event.target.value as string);
-  };
+  const [ship, setShip] = useState(0);
+  const [distance, setDistance] = useState(0);
 
   return(
     <>
@@ -23,23 +22,29 @@ export function IO() {
               id="selectShip"
               labelId="selectShip"
               label="Ship"
-              onChange={handleChange}
-              >
-              <MenuItem value={'Sevastopol'}>Sevastopol</MenuItem>
-              <MenuItem value={'Negev'}>Negev</MenuItem>
-              <MenuItem value={'Nomad'}>Nomad</MenuItem>
-              <MenuItem value={'Lightning'}>Lightning</MenuItem>
+              defaultValue="0"
+              onChange={event => setShip(parseInt(event.target.value))}
+            >
+              <MenuItem value={"0"}>Sevastopol</MenuItem>
+              <MenuItem value={"1"}>Negev</MenuItem>
+              <MenuItem value={"2"}>Nomad</MenuItem>
+              <MenuItem value={"3"}>Lightning</MenuItem>
             </Select>
           </FormControl>
           <TextField
             id="outlined-number"
             label="Distance"
             type="number"
+            onChange={event => setDistance(parseInt(event.target.value))}
             sx={{backgroundColor: 'rgba(40,40,40,0.8)', width: 200}}
           />
         </Stack>
         <Stack spacing={2}>
-          <img src={Sevastopol} alt="" />
+          <img src={
+            ship === 0 ? Sevastopol : 
+            ship === 1 ? Negev :
+            ship === 2 ? Nomad : Lightning
+          } alt="" />
         </Stack>
       </Stack>
     </>
