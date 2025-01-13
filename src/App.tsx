62f719fe156406e2e5e90708d37288a83172c79f
antoniotoni <antonio.gaming10@gmail.com>
2025-01-13 02:18:28 +0100

    Fixed site being scrollable a tiny bit

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { IO } from './components/IO';
 function App () {
 	return (
 		<>
-			<Container sx={{ display: 'flex', flexDirection: 'column', minHeight: '100vh', alignItems: 'center'}}>
+			<Container sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
 				<Typography variant='h3' sx={{ marginTop: 1, marginBottom: 0, color: 'rgb(244, 137, 72)'}}>HIGHFLEET FUEL CALCULATOR</Typography>
 				<div style={{ justifyContent: 'center', alignItems: 'center', flex: '1 0 0%', display: 'flex' }}>
 					<IO/>
